fae41d99d8761f3f0e88517a79714bd2bd453fdf
ky <admin@acchar.com>
2024-03-28 11:58:11 +0800

    2

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include <arpa/inet.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(){
+	int sd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sd < 0) {
+		std::cerr << "socket error" << std::endl;
+		exit(-1);
+	}
+	return 0;
+	//
+}

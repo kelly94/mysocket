2727e1e27b7536d247b61a0f2c53a2ac53a03a00
ky <admin@acchar.com>
2024-03-28 11:59:48 +0800

    2

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <unistd.h>
 
 int main(){
-	int sd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sd < 0) {
+	int S_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (S_fd < 0) {
 		std::cerr << "socket error" << std::endl;
 		exit(-1);
 	}
